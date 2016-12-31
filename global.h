c5dcb27731fb0b48f1a42f341e44df1ceb07a95a
yetoo-main2 <scottbencohen@yahoo.com>
2016-12-30 22:40:53 -0800

    This is still broken if attempt compilation, but added lots and lots of comments and cleaned some stuff up in the source code

--- global.h
+++ global.h
@@ -1,33 +1,34 @@
 //#define KEY_UP 72
 #ifndef GLOBALS_H
 #define GLOBALS_H
-//remove declarations, add to cpp file
+//remove initializations, add to cpp file
+//these are for the command line arguments explained in the main file, it's just easier that way
 bool verbose = false, demigod= false, god = false, interdimensionalplus = false, maxchaos = false, beyondchaosi = false, beyondchaos9ki = false, happy = false, nomenu = false; 
-	int sector = 0;
-	int sectoramnt = 0;
-	int x = 0, y = 0;
-	int moves = 0; 	
-	char c;
-	std::string selectsector = "";
+	int sector = 0; //This is a bit misleading, this is the number porition of the sector
+	int sectoramnt = 0; //Or sector amount, this determines the number of sectors to be generated
+	int x = 0, y = 0; //I have no idea what the hell these are supposed to be for
+	int moves = 0; //this is the variable that holds the current move 	
+	char c; //I think this was supposed to be for that commented out case statement in main.cpp 
+	std::string selectsector = ""; //I think this is useless, but it is the variable that holds the user selection
 	std::string sectorts = ""; //sector to string
-	std::string sectorwl= ""; //sector with letter this is going to contain the added letter and number
+	std::string sectorwl= ""; //***POSSIBLY FIX sector with letter this is going to contain the added letter and number
 	std::string sectorjl=""; //sector with just letter
 	std::string sectorjn=""; //sector with just number
 	std::string sectordec= ""; //string for sector decision 
-	std::string sectorletter[26] =  {"A","B","C","D","E","F","G","H","I","J","K", "L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};	
-	std::string sectorindex[100];
-	std::string desc = " ";
+	std::string sectorletter[26] =  {"A","B","C","D","E","F","G","H","I","J","K", "L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"}; //this is the array that is supposed to hold the predefined letters that sectors will contain 	
+	std::string sectorindex[100]; //the index initialized with 100 spaces maximum
+	std::string desc = " "; //stuff is going to come here soon
 
 #endif
 #ifndef __SCREENEFFECTS__
 #define __SCREENEFFECTS__
 #include <iostream>
-void ClearScreen()
+void ClearScreen() //Clears the screen but scrolls
 {
-	for (int x = 0; x < 5; x++)
+	for (int x = 0; x < 5; x++) //This scrolls 5 times
 	{      
-	std::cout << "\n\n\n\n\n\n\n\n\n\n";
-	usleep(500000); //half a second 
+	std::cout << "\n\n\n\n\n\n\n\n\n\n"; //i don't know if this is lame, but it's pretty lame
+	usleep(500000); //half a second, this is what makes it scroll 
 	}
 }
 #endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
-//If anyone dares to look at this file, compile with -std=c++11
+//If anyone dares to look at this file, compile with -std=c++11 and -lncurses (ncurses not required as of now)
+//If in the possibility of saving to the file, add catch if sudden stop a catch would be ensuring that both the value has inputted and depected ??? or crash
+//make sure that they know you can restart the game without selecting another sector, but they have less of something than they started with, making the option useless in a way to the user 
+/***FIX DUPLICATES IN THE SECTOR INDEX***/
+/****EXPLANATIONS OF THE GLOBAL VARIABLES ARE PROVIDED IN THE global.h FILE LOCATED IN THIS SAME DIRECTORY AS THIS ONE AS OF NOW****/
 #include <iostream>
-#include <stdlib.h> /* prinf(), scanf(), puts, NULL, rand, srand */
+#include <stdlib.h> /* prinf(), scanf(), puts, NULL, rand, srand*/
 #include <stdio.h> /* srand(), rand() */
 #include <time.h> /* time */
 #include <vector> /* vector */
@@ -123,85 +127,73 @@ int main (int argc, char*argv[])
 		endwin();
 	}
 */
-	srand(time(NULL));
-	sectoramnt = rand() % 100 + 1;
+	srand(time(NULL)); //starts the instance of random
+	sectoramnt = rand() % 100 + 1; //sectoramnt equals a random number from 1 to 100 (verify on that)
 	//std::string sectorindex [sectoramnt];
 	//std::cout << "It's nuclear war time!\n";
 	std::cout << "Sectors:\n"; 
-	for (int i = 0; i < sectoramnt; i++)
+/***THE LOOP THAT PUTS VARIABLES INTO THE INDEX STARTS HERE***/
+	for (int i = 0; i < sectoramnt; i++) //for loop iterates for whatever number of times defined by sectoramnt
 	{
-	//std::cout << "test";	
-	sector = rand() % 100 + 1;
-	//TODO add special snowflake status if the program fucks up and causes number fluctuation
-
-        sectorts = std::to_string(sector);
-	//sectorjl = sectorletter[rand()%26+1];
-	//sectorwl = sectorjl + sectorts;
-	//the reason for 26 is because it needs to choose a letter for every value that is in the letter array
-	sectorwl = sectorletter[rand()%26+0] + sectorts;
-        sectorindex[i] = sectorwl;
-	//if in the possibility of saving to the file, add catch if sudden stop 
-	//a catch would be ensuring that both the value has inputted and depected or crash
-	std::cout << sectorindex[i] << " "; 
+		sector = rand() % 100 + 1; //sector equals a random number from 1 to 100 (verify on that) 
+		//TODO add special snowflake status if the program fucks up and causes number fluctuation **fix position of this TODO statement 
+		sectorts = std::to_string(sector); //sectorts, or sector to string, converts the random number that is sector to a string
+		//sectorjl = sectorletter[rand()%26+1];
+		//sectorwl = sectorjl + sectorts;
+		//the reason for 26 is because it needs to choose a letter for every value that is in the letter array
+		sectorwl = sectorletter[rand()%26+0] + sectorts; //sectorwl, or sector with letter, equals a value in the array, sectorletter, that is decided by a randomly generated vaule in the range of 0 to 26 
+		sectorindex[i] = sectorwl; //the array, sectorindex, will iterate the index because of the above for loop. This iteration of the index will equal sectorwl 
+		std::cout << sectorindex[i] << " "; //this is where the output of sectors comes from as since the values have just been inputted into the sectorindex array, they can now be displayed in the same iteration
 	}
-	std::cout << "\n";
+	std::cout << "\n"; //makes a new line for separation
 	
-
-	while (happy == false)
-	//move a lot of what is above into here so the user can have choice
+/***DECISION TIME FOR SELECTING THE RIGHT SECTOR STARTS HERE***/
+	while (happy == false) //this loop is for the user to continually make a decision without possibility or fear being kicked out due to the fault of the program
 	{
-	//make sure that they know you can restart without selecting another secotr, but they have less of something than they started with, making the option useless in a way 
-	std::cout << "Which sector do you want to select?\nNOTE: You cannot select any more sectors after this point in this session, unless of course you choose to restart the session.\n";
-	std::cin >> sectordec;
-	for (int u = 0; u < sectoramnt; u++)
-	{
-	if (sectordec.compare(sectorindex[u]) == 0 && sectorindex[u] != "\0")
-	{
-		std::string happydec = "";
-		std::string happydec2= "";		
-		std::cout << "You selected sector " << sectordec << "\n";
-		selectsector = sectordec;
-			//IT'S LOADING SECTOR INFORMATION TIME!
-			/*
-			General format:
-			---------------
-			Sector Name: <Sector Name>
-			Sector Series: <Sector Letter Series>
-			Sector Location in Series: <Sector Location in Series>			
-			Sector Description: <Varies Depending on Sector Letter Series and Number Within>
-			---------------
-			Verify if this is the sector that the user wants
-			If it is not, go to prompt screen.
-			*/
-			//add the shit above
-			//cause program to scroll to add effect of travel
-			sectorjl = selectsector.substr(0,1);
-			sectorjn = selectsector.substr(1, selectsector.length());			
-
-			std::cout << "Sector Name: " << selectsector << 
-	"\nSector Series: " << sectorjl << "\nSector Location in Series: " << sectorjn << "\nSector Description: " << desc << "\n";
-		std::cout << "Are you sure this is the sector you desire?(y | n)\n";
-		std::cin >> happydec;
-		//make to lowercase
-		//std::transform(happydec.begin(), happydec.end(), happydec.begin(), ::tolower);
-		if (happydec == "y")
+		std::cout << "Which sector do you want to select?\nNOTE: You cannot select any more sectors after this point in this session, unless of course you choose to restart the session. (You can keep the sector that you select into the next restart but you'll be losing <insert waht they'll be losing>)\n";
+		std::cin >> sectordec; //remember, above this input are the generated sectors, now we are deciding\, with the variable sectordec or sector decision, which sector to choose, but in our perspective (not the user's) we are choosing a sector that will then have information extracted with it 
+		for (int u = 0; u < sectoramnt; u++) //this for loop will loop from 0 to whatever number was generated to have sectoramnt to be equal with it
 		{
-			happy = true;
-			std::cout << "Initializing match for selected sector " << selectsector << "...\n";
-			ClearScreen();
-			break;
+			if (sectordec.compare(sectorindex[u]) == 0 && sectorindex[u] != "\0") //**the usefulness of this is questionable, but this will check to see if there are any differences between the sectordec and the sectorindex whom's item index is the same as the current iteration && if the current index of sectorindex is not equal to a space, it's just here just in case something goes wrong up there, but wait hold on ,this is fucking awful, if the decided sector doesn't equal the current index of sectorindex, then it's going to fail and go to this point again until it does equal, meaning that if there was a duplicate somewhere, it would go to the first one, this sucks and should be fixed for fucks sake, I'm not an idiot, I'm just retarded. 
+			{
+				std::string happydec = ""; //happydec is declared and initialized with an empty string for question that is below 
+				std::cout << "You selected sector " << sectordec << "\n"; //reminds the player what they selected for verification and trust of the program, "If the program cnan get this right, then what can it get wrong?"
+				selectsector = sectordec; //**the usefulness of selectsector is questionable but it now equals whatever sectordec equals 
+					/***IT'S LOADING SECTOR INFORMATION TIME!***/
+					/*
+					General format:
+					---------------
+					Sector Name: <Sector Name>
+					Sector Series: <Sector Letter Series>
+					Sector Location in Series: <Sector Location in Series>			
+					Sector Description: <Varies Depending on Sector Letter Series and Number Within>
+					---------------
+					Verify if this is the sector that the user wants
+					If it is not, go to prompt screen.
+					*/
+					sectorjl = selectsector.substr(0,1); //sectorjl or sector just letter consists of the selected sector having substr applied to it in which the selection of the string is 0 to 1 which results in the letter portion of the string
+					sectorjn = selectsector.substr(1, selectsector.length()); //sectorjn or sector just number consists of the selected sector having substr applied to it in which the selection of the string is 1 to the length of selectsector single the number portion could either be single digit, double digit, or triple digit. In case you haven't figured it out by now, this is for the number portion of the string  			
+					std::cout << "Sector Name: " << selectsector << "\nSector Series: " << sectorjl << "\nSector Location in Series: " << sectorjn << "\nSector Description: " << desc << "\n"; //This bit compiles a lot together, This takes the sector name [selectsector] (temporarily selectsector; will eventually change to something more unique), the sector letter [sectorjl] which will identify the sector series (the sectors with the same letter (A-Z) are in the same series), the sector number [sectorjn] which will identify the location in the sector series, and the sector description which is currently blank, but eventually each series (A-Z) will have their own unique description and the description will dynamically change, according to the main description, depending on the location in the series  
+					std::cout << "Are you sure this is the sector you desire?(y | n)\n"; //It's important for verification, it's annoying for new users who experiment to test a method for something in which they are actually testing a "final product". Possibly allow to disable verification through cli arguments
+					std::cin >> happydec; //this is the question that was supposed to come soon, happydec is named happy dec because it makes the user, at least the new user happy for verification 
+					/***add a to lowercase function***/
+					if (happydec == "y") //this currently only checks to see if happydec is equal to the string "y". I think, I may very well be incorrect on this, but since there is only y, if anything else is entered it will just go back to the beginning of this embeded loop.
+					{
+						happy = true; //since the verification is satisfied, happy will now equal true and break out of the main while loop, note how it doesn't break out immediately
+						std::cout << "Initializing match for selected sector " << selectsector << "...\n"; //This is telling the user what is going on 
+						ClearScreen(); //this calls the ClearScreen() function which is specified in global.h, but it's for scrolling which enhances the user experience
+						break; //***DO EXPERIMENTATION this breaks out of the current look but I wonder why since I alrady halled happy = true, need to do experiments to see whether this is worth being here, but it's probably not 
+					}
+			}
 		}
-		
-	}
-	}
 	}	
-	std::cout << "Move: " << moves << "\n";
-	//this for loop is supposed to sort all the letters	
+	std::cout << "Move: " << moves << "\n"; //this displays the current move, this shouldn't be here, it should be in a loop that would increment this
+	/***EXPERIMENTAL SORTING STARS HERE COMMENTATION CEASES***/
 	int a[100];
 	int temp;
 	//crappy excuse for a buffer
 	bool satis = false;
-	for (int i = 0; i < sectoramnt; i++)
+	for (int i = 0; i < sectoramnt; i++) //this for loop is supposed to sort all the letters	
 	{
 		//always going to be equal to the sectorindex		
 		std::string tmpl = ""; //templetter
@@ -238,48 +230,3 @@ int main (int argc, char*argv[])
 	std::cout << "\n";
 	return 0;
 }
-//http://www.cplusplus.com/reference/cstdlib/rand/
-/*
-	for (int x = 0; x < 26; x++)
-	{
-		std::cout << "\n";
-		for (int z = 0; z < 101; z++)
-		{ 			
-			sectorts = std::to_string(z);
-			sectorwl = sectorletter[x] + sectorts;					
-			//std::cout << sectorwl << " ";			
-			//std::string *sectorindexfind = std::find(std::begin(sectorindex), std::end(sectorindex), sectorwl);		
-			
-			if (sectorindexfind != std::end(sectorindex))
-			{
-				std::cerr << "found at position" << std::distance(sectorindex, sectorindexfind) << "\n";
-			}
-			else
-			{	
-				std::cerr << sectorwl << "\n";	
-			}
-			
-			for (int y = 0; y < sectoramnt; y++)
-			{			
-			if (sectorwl.compare(sectorindex[y]) == 0)
-			//std::cout << "a";
-			//if (sectorwl.compare(sectorindex[y]) != 0)
-			std::cout << sectorwl << " ";
-			
-			//else
-			//std::cout << sectorwl << " ";
-			}
-			
-		}  
-	}
-		
-	
-
-	
-    //std::string * sectordecfind;
-	bool sectordecfind (const std::string $sectordec, const std::vector<std::string> &sectorindex);
-	{
-			//return std::find(sectorindex.begin(), sectorindex.end(), sectordec) != sectorindex.end());
-			return std::find(sectorindex.begin, sectorindex.end, sectordec) != sectorindex.end;
-	}
-	*/
